d5db882d1ac4db183c750bfc20fe4b8239b0bbd7
Nurul Fatihin <fatihinsahid@gmail.com>
2015-03-18 01:11:49 +0800

    change database, add new model which is admin.php

---
+++ application/models/admin.php
@@ -0,0 +1,36 @@
+<?php
+
+class Admin extends MY_Model {
+    
+    const DB_TABLE = 'admin';
+    const DB_TABLE_PK ='admin_id';
+    //const DB_TABLE_PK ='admin_name';
+    //const DB_TABLE_PK ='admin_username';
+    //const DB_TABLE_PK ='admin_password';
+
+    /**
+    * Admin uniques identifier
+    * @var int 
+    */
+   public $admin_id ;
+   
+   /**
+    * Admin name
+    * @var varchar 
+    */
+   public $admin_name;
+   
+   /**
+    * Admin username
+    * @var varchar 
+    */
+   public $admin_username;
+   
+   /**
+    * Admin password
+    * @var varchar 
+    */
+   public $admin_password;
+   
+}
+
