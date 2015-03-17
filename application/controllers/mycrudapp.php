67c9ee99f6db474efeb24e56e94daf2895b11925
Nurul Fatihin <fatihinsahid@gmail.com>
2015-03-18 01:13:58 +0800

    altering database

--- application/controllers/mycrudapp.php
+++ application/controllers/mycrudapp.php
@@ -2,7 +2,7 @@
 
 class Mycrudapp extends CI_Controller {
 
-    private $pictures;
+    private $admin;
     private $users;
 
     /**
@@ -10,14 +10,25 @@ class Mycrudapp extends CI_Controller {
      */
     public function index(){
         $this->load->model('Users');
-        $this->Users->username = 'nurul';
+        $this->Users->admin_id = '1';
+        $this->Users->firstname = 'Mohd';
+        $this->Users->lastname = 'Sahid';
+        $this->Users->username = 'sahid';
+        $this->Users->address = 'jalan setia jaya';
+        $this->Users->mobile_no = '102-3074774';
+        $this->Users->email = 'nurul@yahoo.com';
+        $this->Users->password = '51mohd';
+        $this->Users->datestart = '2013-09-15';
+        $this->Users->status = 'active';
         $this->Users->save();
         echo '<tt><pre>' . var_export($this->Users, TRUE) . '</tt></pre>';
         
-        $this->load->model('Pictures');
-        $this->Pictures->caption = 'aku dah pening';
-        $this->Pictures->save();
-        echo '<tt><pre>'. var_export($this->pictures, TRUE).'</tt></pre>';
+        $this->load->model('Admin');
+        $this->Admin->admin_name = 'Jennifer';
+        $this->Admin->admin_username = 'jen';
+        $this->Admin->admin_password = '000000';       
+        $this->Admin->save();
+        echo '<tt><pre>'. var_export($this->admin, TRUE).'</tt></pre>';
         
         $this->load->view('mycrudapps') ;
     }
--- application/models/users.php
+++ application/models/users.php
@@ -12,10 +12,88 @@ class Users extends MY_Model {
    public $id ;
    
    /**
+     * Users unifying reference
+     * @var int
+     */
+    public $admin_id;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $firstname;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $lastname;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $address;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $mobile_no;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $home_no;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $email;
+   
+   /**
     * Users username
     * @var varchar 
     */
    public $username;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $password;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $datestart;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $status;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $modified;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $remarks;
+   
+   /**
+    * Users username
+    * @var varchar 
+    */
+   public $picture;
    
 }
 
