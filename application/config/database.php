11e86041b4935c684421e91c3315aa6f87039288
Nurul Fatihin <fatihinsahid@gmail.com>
2015-03-17 14:33:19 +0800

    add username, pasword and database in database.php

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'mycrud';
+$db['default']['password'] = 'mycrud';
+$db['default']['database'] = 'mycrudapp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
