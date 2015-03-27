386759c9c505e27be330ca9022ad1c2bcd771045
hanafiah <ibnuyahya@gmail.com>
2015-03-27 11:05:00 +0800

    code review

--- application/views/users_login.php
+++ application/views/users_login.php
@@ -2,37 +2,38 @@
     <head>
         <title>User Login</title>   
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap-theme.min.css'); ?>">
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
     </head>
     <body>
-        <?php
-        if ($this->session->flashdata('message')) {
-            echo $this->session->flashdata('message');
-        }
-        ?>
+        <div class='container'>
+            <?php
+            if ($this->session->flashdata('message')) {
+                echo '<div class="alert alert-danger" role="alert">' . $this->session->flashdata('message') . '</div>';
+            }
+            ?>
 
-        <?php echo validation_errors(); ?>
-        <?php echo form_open('user/user_login_process') ?>
+            <?php echo validation_errors('<div class="alert alert-danger" role="alert">', '</div>'); ?>
+            <?php echo form_open('user/user_login_process') ?>
+            <h1> Staff Login</h1>
+            <form class="form" method="post">
+                <div class="form-group">
+                    <label>Username :</label>
+                    <input class="form-control" type="text" name="username" value="" />
+                </div>
 
-        <h1> Staff Login</h1>
-        <form method="post">
-            <p>
-                <label>Username :</label>
-                <input type="text" name="username" value=""/>
-            </p>
+                <div class="form-group">
+                    <label>Password :</label>
+                    <input class="form-control" type="password" name="password" value=""/>
+                </div>
 
-            <p>
-                <label>Password :</label>
-                <input type="password" name="password" value=""/>
-            </p>
-
-            <p>
-                <input type="submit" value="Login" name="submit"/>
-            </p>
-
-            <p>
-                <a href="user/create">To SignUp Click Here</a>
-            </p>
-        </form>
-        <?php echo form_close(); ?>
+                <div class="form-group">
+                    <input class="btn btn-primary" type="submit" value="Login" name="submit"/>
+                    <a class="btn btn-default" href="user/create">To Sign Up Click Here</a>
+                </div>
+            </form>
+            <?php echo form_close(); ?>
+        </div>
     </body>
 </html>
