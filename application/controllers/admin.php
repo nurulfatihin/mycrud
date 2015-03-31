f04d2e67f38d8872fffb197f89108480d8070cd0
Nurul Fatihin <fatihinsahid@gmail.com>
2015-03-31 12:59:59 +0800

    alphabetic

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -56,8 +56,8 @@ class Admin extends CI_Controller {
          */
         if (isset($_POST['submit'])) {
             $this->form_validation->set_rules('admin_id', 'Admin ID', 'required');
-            $this->form_validation->set_rules('firstname', 'First name', 'required|callback__alpha_dash_space');
-            $this->form_validation->set_rules('lastname', 'Last name', 'required|callback__alpha_dash_space');
+            $this->form_validation->set_rules('firstname', 'First name', 'required|callback__alpha_dash_space|alpha');
+            $this->form_validation->set_rules('lastname', 'Last name', 'required|callback__alpha_dash_space|alpha');
             $this->form_validation->set_rules('address', 'Address', 'required');
             $this->form_validation->set_rules('mobile_no', 'Mobile no.', 'required');
             $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
